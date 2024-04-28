a3b2250d1a3a1bf3aa36d025142978d4337a0de8
Luna <Luna@LUSPC>
2024-04-28 19:43:34 +0200

    Add project files.

---
+++ TestRace/Models/ParticipantEntry.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRace
+{
+    public class ParticipantEntry
+    {
+        private string _name;
+        private int _id;
+        private TimeOnly _startTime;
+        private TimeOnly _finishTime;
+        private string _raceType;
+        private string _fullEntryString;
+
+        public  string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public  int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public  TimeOnly StartTime
+        {
+            get { return _startTime; }
+            set { _startTime = value; }
+        }
+
+        public  TimeOnly FinishTime
+        {
+            get { return _finishTime; }
+            set { _finishTime = value; }
+        }
+
+        public  string RaceType
+        {
+            get { return _raceType; }
+            set { _raceType = value; }
+        }
+
+        public string FullEntryString
+        {
+            get { return _fullEntryString; }
+            set { _fullEntryString = value; }
+        }
+
+
+
+    }
+}
---
+++ TestRace/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        TimeOnly timeOnly = new(10, 10, 10);
+
+        string line;
+        try
+        {
+            //Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new("C:\\Users\\Luna\\Desktop\\race-results.txt");
+            //Read the first line of text
+            line = sr.ReadLine();
+            //Continue to read until you reach end of file
+            Console.WriteLine(timeOnly.ToString("HH:mm:ss"));
+            while (line != null)
+            {
+                //write the line to console window
+                Console.WriteLine(line);
+                string[] entryValues = line.Split(",");
+                if (entryValues.Length > 0)
+                {
+                    foreach (string entryValue in entryValues)
+                    {
+                        Console.WriteLine(entryValue);
+                    }
+                }
+                //Read the next line
+                line = sr.ReadLine();
+            }
+            //close the file
+            sr.Close();
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+    }
+}
