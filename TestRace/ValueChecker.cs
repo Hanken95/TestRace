694ae0df17268f8a99c3fb198cc9f42e5aa39301
Luna L <hanken95@gmail.com>
2024-04-28 21:16:03 +0200

    Start of EntireEntryChecker

--- TestRace/ValueChecker.cs
+++ TestRace/ValueChecker.cs
@@ -14,12 +14,31 @@ namespace TestRace
 
     public static class ValueChecker
     {
+        public static List<Fault> EntireEntryChecker(string[] entry)
+        {
+            List<Fault> faults = new List<Fault>();
+            //if (entry.Length != 5)
+            //{
+            //    CheckLenghtOfEntryProblem(entry);
+            //    return faults;
+            //}
+            if (NameChecker(entry[0]).Count > 0)
+            {
+                foreach (var fault in NameChecker(entry[0]))
+                {
+                    faults.Add(fault);
+                }
+            }
+            return faults;
+        }
+
         public static List<Fault> NameChecker(string name)
         {
             List<Fault> faults = new List<Fault>();
             if (string.IsNullOrWhiteSpace(name))
             {
                 faults.Add(Fault.NameMissing);
+                return faults;
             }
             if (name.All(char.IsLetter))
             {
@@ -27,5 +46,6 @@ namespace TestRace
             }
             return faults;
         }
+
     }
 }
